f1e2b40a10648239ae7d41d683baf70cc13cfb13
CJ-Bush <caca.pascual.up@phinmaed.com>
2023-10-17 15:54:40 +0800

    [update] ui, itemlist

--- app/src/main/java/com/example/canteenapplication/studentpage/Studentpage.kt
+++ app/src/main/java/com/example/canteenapplication/studentpage/Studentpage.kt
@@ -1,10 +1,11 @@
-package com.example.canteenapplication
+package com.example.canteenapplication.studentpage
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.canteenapplication.R
 
 
 class Studentpage : Fragment() {
